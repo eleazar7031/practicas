eb8dff5b7454f6c283d32246cd2b7cfd1adeae69
eleazar7031 <00083925@uca.edu.sv>
2025-06-03 09:51:48 -0600

    hola

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main(){
-    
+int f(int x){
+    int valor = pow(x , 2) + 2;
+    return valor;
+}
+int g(int x, int y){
+
+    int valor = 2*x - 3*y;
+    return valor;
+}
+
+void cosito(int n){
+    cout<<n<<"\n";
+}
+int main(void){
+    //funcion pre-definida
+    int potencia = pow(2,8); // 2^8 
 
 
 
